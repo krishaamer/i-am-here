86fb6662b865867b4892fac057311fb38bec7388
Kris Haamer <kris@haam.co>
2023-04-15 17:26:31 +0900

    Update styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,23 +6,33 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function ChatSupportTest() {
   const theme: ITheme = {
-    bgColorPrimary: "#efefef",
-    bgColorSecondary: "#efefef",
-    textColorPrimary: "#ffffff",
-    textColorSecondary: "#ffffff",
-    btnColorPrimary: "#246724",
-    btnColorSecondary: "#ffffff",
-    border: "none",
-    borderRadius: "0",
-    moduleColor: "#efefef",
+    bgColorPrimary: "#FFB4EF",
+    bgColorSecondary: "#FFB4EF",
+    textColorPrimary: "#fff",
+    textColorSecondary: "#fff",
+    btnColorPrimary: "#FFB4EF",
+    btnColorSecondary: "#fff",
+    border: "1px solid #FFB4EF",
+    borderRadius: "10px",
+    moduleColor: "#fff",
   };
 
   return (
-    <Chat
-      account="0x3A6666e9d8fc42d903807EF35c713527223b1ce3"
-      supportAddress="0x3A6666e9d8fc42d903807EF35c713527223b1ce3"
-      env="staging"
-      theme={theme}
-    />
+    <div>
+      <div className="container mx-auto">
+        <div className="header grid h-screen place-items-center">
+          <h1 className="text-5xl inter">I AM HERE.</h1>
+        </div>
+      </div>
+      <div className="chat-container inter">
+        <Chat
+          account="0x3A6666e9d8fc42d903807EF35c713527223b1ce3"
+          supportAddress="0x3A6666e9d8fc42d903807EF35c713527223b1ce3"
+          env="staging"
+          modalTitle="I AM HERE."
+          theme={theme}
+        />
+      </div>
+    </div>
   );
 }
