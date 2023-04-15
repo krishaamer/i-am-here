043bd87d77b1e9e56c8084c9b118a22efdf504ad
Kris Haamer <kris@haam.co>
2023-04-15 17:40:20 +0900

    Add fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Chat, ITheme } from "@pushprotocol/uiweb";
-import { Inter } from "next/font/google";
+import { Inter, Sora, Gloria_Hallelujah } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+const zora = Sora({ subsets: ["latin"] });
+const gloria = Gloria_Hallelujah({ subsets: ["latin"], weight:["400"] });
 
 export default function ChatSupportTest() {
   const theme: ITheme = {
@@ -21,13 +23,14 @@ export default function ChatSupportTest() {
     <div>
       <div className="container mx-auto">
         <div className="header grid h-screen place-items-center">
-          <h1 className="text-5xl inter">I AM HERE.</h1>
+          <h1 className="text-5xl gloria">I AM HERE.</h1>
         </div>
       </div>
       <div className="chat-container inter">
         <Chat
           account="0x3A6666e9d8fc42d903807EF35c713527223b1ce3"
           supportAddress="0x3A6666e9d8fc42d903807EF35c713527223b1ce3"
+          // @ts-ignore: Unreachable code error
           env="staging"
           modalTitle="I AM HERE."
           theme={theme}
